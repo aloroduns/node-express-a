b793c29d706ef6d6b21fb94dbe4b2c53f105782c
mantisdev20 <aloroduns@yahoo.com>
2024-07-01 01:14:09 -0400

    answers for lesson 1

--- 01-node-tutorial/answers/02-globals.js
+++ 01-node-tutorial/answers/02-globals.js
@@ -1,7 +1,8 @@
 //set MY_VAR="H1 there! for windows"
+// __dirname - path to current directory
 
 console.log(__dirname);
 
 setInterval(() => {
-    console.log('hello world')
+  console.log('hello world')
 }, 1000)
